a8cc80b54ed4f0b1374ecde4c9daeedb54fec775
ageu_dev19 <ageumenezes423@gmail.com>
2024-01-01 23:12:16 -0300

    Testing

--- App.tsx
+++ App.tsx
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00b454',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Text: {
+    color: 'red',
+  }
 });
